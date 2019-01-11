3ec12e752fe3bc60825549671f7e9a55e8967e02
Allison <allison.tharp@gmail.com>
2019-01-11 12:04:27 -0500

    Fixed issue where the same team name existed in multiple leagues.

--- Main.ps1
+++ Main.ps1
@@ -1,4 +1,4 @@
-. "\funcs.ps1"
+. "funcs.ps1"
 
 $PeoplesTeams = New-Object system.Collections.ArrayList
 
@@ -7,16 +7,26 @@ $MyTeams = New-Object System.Collections.ArrayList
 $MyTeams +=  @{League = "NHL"; Team = "Tampa Bay Lightning"; Color1 = "blue"; Color2 = "white"}
 $MyTeams +=  @{League = "NFL"; Team = "IND"; Color1 = "blue"; Color2 = "white"}
 
-$PeoplesTeams += @{Name="Person1"; Key="Key1"; Teams=$MyTeams}
+
+$PeoplesTeams += @{Name="Person1"; Key="LifxKey1"; Teams=$MyTeams}
 
 
 $DadsTeams = New-Object System.Collections.ArrayList
 $DadsTeams +=  @{League = "NFL"; Team = "IND"; Color1 = "blue"; Color2 = ""}
-$PeoplesTeams += @{Name="Person2"; Key="Key2"; Teams=$DadsTeams}
+$DadsTeams +=  @{League = "NBA"; Team = "IND"; Color1 = "blue"; Color2 = "yellow"}
+$PeoplesTeams += @{Name="Person2"; Key="LifxKey2"; Teams=$DadsTeams}
+
+# Get a key for League - Team
+foreach ($person in $PeoplesTeams){
+    foreach($team in $person.teams){
+        $LeagueTeam = "$($team.League) - $($team.team)"   
+        $team | Add-Member -MemberType NoteProperty -Name LeagueTeam -Value $LeagueTeam
+    }
+}
 
 
-$TeamsToMonitor = $PeoplesTeams.teams.team
-$LeaguesToMonitor = $PeoplesTeams.teams.League
+$TeamsToMonitor = $PeoplesTeams.Teams.LeagueTeam | sort -Unique
+$LeaguesToMonitor = $PeoplesTeams.Teams.League | sort -Unique
 
 $CurrentScores = get-CurrentScores -OnlyMonitoredTeams 1 -OnlyActiveGames 1 -TeamsToMonitor $TeamsToMonitor -LeaguesToMonitor $LeaguesToMonitor
 $PreviousScores = $CurrentScores
@@ -30,10 +40,10 @@ while ($RunTime -le $TimeToRun*60){
     foreach($person in $PeoplesTeams){
         $MyTeams = $person.teams
         $Scores = New-Object system.Collections.ArrayList
-        $Scores += compare-Scores -PreviousScores $($PreviousScores | Where-Object {$_.home -in $MyTeams.Team -or $_.away -in $MyTeams.Team}) `
-            -CurrentScores $($CurrentScores | Where-Object {$_.home -in $MyTeams.Team -or $_.away -in $MyTeams.Team}) -MyTeams $($person.Teams)
+        $Scores += compare-Scores -PreviousScores $($PreviousScores | Where-Object {$_.home -in $MyTeams.LeagueTeam -or $_.away -in $MyTeams.LeagueTeam}) `
+            -CurrentScores $($CurrentScores | Where-Object {$_.home -in $MyTeams.LeagueTeam -or $_.away -in $MyTeams.LeagueTeam}) -MyTeams $($person.Teams)
 
-        invoke-ScoreBird -Scores $Scores -MyTeams $MyTeams -token $token
+        invoke-ScoreBird -Scores $Scores -MyTeams $MyTeams -token $person.key
     }
 
     $PreviousScores = $CurrentScores
--- funcs.ps1
+++ funcs.ps1
@@ -3,7 +3,11 @@
         - Leagues to add:
             - https://gist.github.com/akeaswaran/b48b02f1c94f873c6655e7129910fc3b
             - Men's College Basketball: http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard
-            - Men's College Football: http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard 
+            + Men's College Football: http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard 
+            - ECHL
+            + NBA
+        - only trigger IND if it is IND and NBA (and not if I have IND NFL for pacers)
+        - turn lights on after blinking
 #>
 
 
@@ -24,9 +28,9 @@ function get-NFLScores(){
         $gamesObject | Add-Member -MemberType NoteProperty -Name League -Value "NFL"
         $gamesObject | Add-Member -MemberType NoteProperty -Name GameStatus -Value $($nflObject.qtr)
         
-        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value $($nflObject.home.abbr)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value "NFL - $($nflObject.home.abbr)"
         $gamesObject | Add-Member -MemberType NoteProperty -Name HomeScore -Value $($nflObject.home.score.T)
-        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value $($nflObject.away.abbr)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value "NFL - $($nflObject.away.abbr)"
         $gamesObject | Add-Member -MemberType NoteProperty -Name AwayScore -Value $($nflObject.away.score.T)
 
         $GamesArray += $gamesObject
@@ -76,10 +80,10 @@ function get-NHLScores(){
         $gamesObject | Add-Member -MemberType NoteProperty -Name League -Value "NHL"
         $gamesObject | Add-Member -MemberType NoteProperty -Name GameStatus -Value $($game.status.abstractGameState)
         
-        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value $($game.teams.home.team.name)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value "NHL - $($game.teams.home.team.name)"
         $gamesObject | Add-Member -MemberType NoteProperty -Name HomeScore -Value $($game.teams.home.score)
 
-        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value $($game.teams.away.team.name)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value "NHL - $($game.teams.away.team.name)"
         $gamesObject | Add-Member -MemberType NoteProperty -Name AwayScore -Value $($game.teams.away.score)
 
         $GamesArray += $gamesObject
@@ -104,9 +108,9 @@ function get-NCAAFScores(){
         $gamesObject | Add-Member -MemberType NoteProperty -Name League -Value "NCAAF"
         $gamesObject | Add-Member -MemberType NoteProperty -Name GameStatus -Value $($game.status.type.shortDetail)
         
-        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'home'}).team.abbreviation)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value "NCAAF - $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'home'}).team.abbreviation)"
         $gamesObject | Add-Member -MemberType NoteProperty -Name HomeScore -Value $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'home'}).score)
-        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'away'}).team.abbreviation)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value "NCAAF - $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'away'}).team.abbreviation)"
         $gamesObject | Add-Member -MemberType NoteProperty -Name AwayScore -Value $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'away'}).score)
 
         $GamesArray += $gamesObject
@@ -114,12 +118,38 @@ function get-NCAAFScores(){
     return $gamesArray
 }
 
+function get-NBAScores(){
+    $gamesArray = New-Object System.Collections.ArrayList
+
+    $url = "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"
+    $scores = Invoke-WebRequest -Uri $url | ConvertFrom-Json
+
+    $gameIDs = $scores.events | Select-Object id
+
+    foreach ($gameID in $gameIDs){
+        $gameID = $($gameID.id)
+        $game = $scores.events | Where-Object {$_.id -eq $gameID}
+        $gamesObject = New-Object System.Object
+
+        $gamesObject | Add-Member -MemberType NoteProperty -Name GameID -Value $gameID
+        $gamesObject | Add-Member -MemberType NoteProperty -Name League -Value "NBA"
+        $gamesObject | Add-Member -MemberType NoteProperty -Name GameStatus -Value $($game.status.type.shortDetail)
+        
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Home -Value "NBA - $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'home'}).team.abbreviation)"
+        $gamesObject | Add-Member -MemberType NoteProperty -Name HomeScore -Value $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'home'}).score)
+        $gamesObject | Add-Member -MemberType NoteProperty -Name Away -Value "NBA - $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'away'}).team.abbreviation)"
+        $gamesObject | Add-Member -MemberType NoteProperty -Name AwayScore -Value $(($game.competitions[0].competitors | Where-Object {$_.homeAway -eq 'away'}).score)
+
+        $GamesArray += $gamesObject
+    }
+    return $gamesArray
+}
 
 function invoke-ScoreBird ($Scores, $myTeams, $token){
     foreach($score in $Scores){
-        if($score.TeamName -in $myTeams.team){
+        if($score.TeamName -in $myTeams.LeagueTeam){
             "Yay! $($score.TeamName) scores!"
-            invoke-Pulse -primaryColor ($MyTeams | Where-Object {$_.team -eq $($score.TeamName)}).color1 -secondaryColor ($MyTeams | Where-Object {$_.team -eq $($score.TeamName)}).color2 -numberBlinks $score.Points -token $token
+            invoke-Pulse -primaryColor ($MyTeams | Where-Object {$_.LeagueTeam -eq $($score.TeamName)}).color1 -secondaryColor ($MyTeams | Where-Object {$_.LeagueTeam -eq $($score.TeamName)}).color2 -numberBlinks $score.Points -token $token
         } else {
             "Boo! $($score.TeamName) Score!"
             invoke-Breathe -primaryColor "red" -token $token   
@@ -134,8 +164,10 @@ function get-CurrentScores($OnlyMonitoredTeams, $OnlyActiveGames, $TeamsToMonito
         {$CurrentScores = get-NFLScores}
     if ("NHL" -in $LeaguesToMonitor)
         {$CurrentScores += get-NHLScores}
-    if ("NCAAF -in $LeaguesToMonitor")
+    if ("NCAAF" -in $LeaguesToMonitor)
         {$CurrentScores += get-NCAAFScores}
+    if ("NBA" -in $LeaguesToMonitor)
+        {$CurrentScores += get-NBAScores}
     
     if($OnlyMonitoredTeams -eq 1 -and $OnlyActiveGames -eq 1){
         $CurrentScores = $CurrentScores| Where-Object {($_.Home -in $($TeamsToMonitor) -or $_.Away -in $($TeamsToMonitor)) -and $_.GameStatus -ne "Final"}
